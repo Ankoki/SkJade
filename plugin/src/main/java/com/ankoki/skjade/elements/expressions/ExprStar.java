c8d837a88875e92d7238bc7cc2d3f2c6438aee61
Ankoki-Dev <ankoki.mc@gmail.com>
2021-01-29 13:11:40 +0000

    worked a bit on ExprStar but i cant do maths clearly
    Took 1 hour 10 minutes

--- plugin/src/main/java/com/ankoki/skjade/elements/expressions/ExprStar.java
+++ plugin/src/main/java/com/ankoki/skjade/elements/expressions/ExprStar.java
@@ -12,37 +12,37 @@ import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import org.bukkit.Location;
 import org.bukkit.event.Event;
+import org.bukkit.util.Vector;
 import org.eclipse.jdt.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Name("Star")
 @Description("Returns the points of the outline of a star from the center, radius, and density")
-@Examples("show happy villager at star at block 8 in front of player with radius 10 and density 5")
+@Examples("show happy villager at star at block 8 in front of player with 5 points, with radius 10 and density 5")
 @Since("1.0.0")
 public class ExprStar extends SimpleExpression<Location> {
 
     static {
         Skript.registerExpression(ExprStar.class, Location.class, ExpressionType.SIMPLE,
-                "[(all [[of] the]|the)] [(loc[ation]s|points) of] [a] star (at|from) %location% (with|of) [a] radius [of] %number%(,| and) [a] density [of] %number%");
+                "[(all [[of] the]|the)] [(loc[ation]s|points) of] [a] star (at|from) %location% with %integer% points, (with|of) [a] radius [of] %number%(,| and) [a] density [of] %number%");
     }
 
     private Expression<Location> center;
+    private Expression<Integer> points;
     private Expression<Number> radius;
     private Expression<Number> density;
 
     @Nullable
     @Override
     protected Location[] get(Event event) {
-        //need to google this and have no wifi rn but maths is what
-        /*
-		loop 5 times:
-			set {_p::%loop-number%} to expr-1 ~ (cylindrical vector expr-2, (loop-number * 72), 0)
-		set {_l::*} to (3, 4, 4, 5 and 5)
-		loop {_l::*}:
-			set {_i} to (loop-index parsed as number)th element of (1, 1, 2, 2 and 3)
-			set {_r::*} to {_r::*} and line between {_p::%{_i}%} and {_p::%loop-value%} with density expr-3
-		return {_r::*}
-         */
-        return new Location[0];
+        Location c = center.getSingle(event);
+        int p = points.getSingle(event);
+        double r = radius.getSingle(event).doubleValue();
+        double d = density.getSingle(event).doubleValue();
+        if (c == null) return null;
+        return (Location[]) getStar(c, r, p, d).toArray();
     }
 
     @Override
@@ -64,8 +64,44 @@ public class ExprStar extends SimpleExpression<Location> {
     @Override
     public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, ParseResult parseResult) {
         center = (Expression<Location>) exprs[0];
-        radius = (Expression<Number>) exprs[1];
-        density = (Expression<Number>) exprs[2];
+        points = (Expression<Integer>) exprs[1];
+        radius = (Expression<Number>) exprs[2];
+        density = (Expression<Number>) exprs[3];
         return true;
     }
+
+    private List<Location> getStar(Location origin, double radius, int vertices, double density) {
+        List<Location> locations = new ArrayList<>();
+        double points = ((vertices * (vertices + 1D) / 2D - (vertices * 2) * density));
+        double delta = 360D/vertices;
+        List<Location> vertex = new ArrayList<>();
+        for (int i = 1; i <= vertices; i++) {
+            vertex.add(origin.add(new Vector(Math.cos(delta * i), 0, Math.cos(delta * i)).multiply(radius)));
+        }
+        int index = 0;
+        for (Location location : vertex) {
+            index++;
+            List<Integer> neighbors = new ArrayList<>();
+            neighbors.add(index < vertices ? index++ : 1);
+            neighbors.add(index > 1 ? index-- : vertices);
+            for (int it = 0; it <= vertices; it++) {
+                if (neighbors.contains(it)) {
+                    locations = (getLine(location, vertex.get(it--), density));
+                }
+            }
+        }
+        return locations;
+    }
+
+    private List<Location> getLine(Location start, Location end, double density) {
+        List<Location> locations = new ArrayList<>();
+        double points = start.distance(end);
+        Vector delta = (start.toVector().subtract(end.toVector())).multiply(1/points);
+        for (int i = 1; i <= points; i++) {
+            locations.add(start.add(delta.multiply(new Vector(i, i, i))));
+        }
+        return locations;
+    }
 }
+
+//!show happy villager at star at block 8 in front of player with 5 points, with radius 10 and density 5
