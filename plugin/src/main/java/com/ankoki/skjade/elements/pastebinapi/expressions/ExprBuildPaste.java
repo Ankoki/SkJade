31b7d4dbc2e8a1c479fcc3c66f3a07b5a242d90e
Ankoki-Dev <ankoki.mc@gmail.com>
2021-02-09 02:27:24 +0000

    small fix where i was dumb and got an example wrong lololololl
    Took 4 minutes

--- plugin/src/main/java/com/ankoki/skjade/elements/pastebinapi/expressions/ExprBuildPaste.java
+++ plugin/src/main/java/com/ankoki/skjade/elements/pastebinapi/expressions/ExprBuildPaste.java
@@ -17,7 +17,7 @@ import org.jetbrains.annotations.Nullable;
 
 @Name("Build Paste")
 @Description("Create's a pastebin paste. Returns the link of the built pastebin. Only call this once per builder and store the link. The developer key is linked to your pastebin account and found at this website: https://pastebin.com/doc_api#1")
-@Examples("send \"the url of the built paste with the id \"myPaste\" with the developer key \"{@developerKey}\"")
+@Examples("send the url of the built paste with the id \"myPaste\" with the developer key \"{@developerKey}\"")
 @Since("1.0.0")
 public class ExprBuildPaste extends SimpleExpression<String> {
 
