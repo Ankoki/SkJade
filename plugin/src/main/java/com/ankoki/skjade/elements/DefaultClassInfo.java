f880713425f848743da948c266e57e8de56f6b07
Ankoki-Dev <ankoki.mc@gmail.com>
2021-02-25 01:23:07 +0000

    characters ezpz

---
+++ plugin/src/main/java/com/ankoki/skjade/elements/DefaultClassInfo.java
@@ -0,0 +1,18 @@
+package com.ankoki.skjade.elements;
+
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.registrations.Converters;
+
+public class DefaultClassInfo {
+
+    static {
+        Classes.registerClass(new ClassInfo<>(Character.class, "Character")
+        .user("char(acter)?s?")
+        .name("Character")
+        .description("A single character.")
+        .since("insert version"));
+
+        Converters.registerConverter(Character.class, String.class, String::valueOf);
+    }
+}
---
+++ plugin/src/main/java/com/ankoki/skjade/elements/expressions/ExprCharacter.java
@@ -0,0 +1,48 @@
+package com.ankoki.skjade.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprCharacter extends SimpleExpression<Character> {
+
+    static {
+        Skript.registerExpression(ExprCharacter.class, Character.class, ExpressionType.SIMPLE,
+                "'<(\\S$)>'");
+    }
+
+    private String string;
+
+    @Nullable
+    @Override
+    protected Character[] get(Event e) {
+        if (string == null || string.isEmpty()) return null;
+        return new Character[]{string.toCharArray()[0]};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends Character> getReturnType() {
+        return Character.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "'" + string + "'";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        string = parseResult.regexes.get(0).group(1);
+        return true;
+    }
+}
