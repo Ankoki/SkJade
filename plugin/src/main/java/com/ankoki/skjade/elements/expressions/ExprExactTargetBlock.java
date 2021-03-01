05b192f04da62451bb893d77a376d01b57f56326
Ankoki-Dev <ankoki.mc@gmail.com>
2021-03-01 00:18:44 +0000

    ive literally not tested any of my recent pushes i rly need to

---
+++ plugin/src/main/java/com/ankoki/skjade/elements/expressions/ExprExactTargetBlock.java
@@ -0,0 +1,69 @@
+package com.ankoki.skjade.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptConfig;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Exact Target Block")
+@Description("Gets the exact target block of player.")
+@Examples("set {-target::%player's uuid%} to player's exact target block")
+@Since("insert version")
+public class ExprExactTargetBlock extends SimpleExpression<Block> {
+
+    static {
+        Skript.registerExpression(ExprExactTargetBlock.class, Block.class, ExpressionType.SIMPLE,
+                "%player%'s exact target[ed] block",
+                "%player%'s exact target[ed] block including (1¦source|) fluids");
+    }
+
+    private Expression<Player> player;
+    private FluidCollisionMode mode = FluidCollisionMode.NEVER;
+
+    @Nullable
+    @Override
+    protected Block[] get(Event e) {
+        if (player == null) return null;
+        Player p = player.getSingle(e);
+        if (p == null) return null;
+        return new Block[]{p.getTargetBlockExact(SkriptConfig.maxTargetBlockDistance.value(), mode)};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends Block> getReturnType() {
+        return Block.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return player.toString(e, debug) + "'s exact target block";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, Kleenean isDelayed, ParseResult parseResult) {
+        if (i == 1 && parseResult.mark == 1) {
+            mode = FluidCollisionMode.SOURCE_ONLY;
+        } else if (i == 1) {
+            mode = FluidCollisionMode.ALWAYS;
+        }
+        player = (Expression<Player>) exprs[0];
+        return true;
+    }
+}
