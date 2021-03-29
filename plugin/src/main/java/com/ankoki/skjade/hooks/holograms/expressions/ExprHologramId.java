0ec858a4ae336d57cf96d56133d1a37effb74a53
Ankoki <72931234+Ankoki-Dev@users.noreply.github.com>
2021-03-29 19:26:17 +0100

    gimmie gimmie moar gimmie moar why this wasnt in the initial release whenn i had a method for it is a mystery too me.
    Took 7 minutes

---
+++ plugin/src/main/java/com/ankoki/skjade/hooks/holograms/expressions/ExprHologramId.java
@@ -0,0 +1,60 @@
+package com.ankoki.skjade.hooks.holograms.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import com.ankoki.skjade.hooks.holograms.HologramManager;
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("ID of a Hologram")
+@Description("Returns the ID of a hologram")
+@Examples("broadcast event-hologram's id")
+@Since("1.2.0")
+public class ExprHologramId extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(ExprHologramId.class, String.class, ExpressionType.SIMPLE,
+                "([the] (id|key) of [the [hologram]] %-hologram%|%hologram%[']s (id|key))");
+    }
+
+    private Expression<Hologram> hologram;
+
+    @Nullable
+    @Override
+    protected String[] get(Event e) {
+        if (hologram == null) return null;
+        Hologram holo = hologram.getSingle(e);
+        if (holo == null) return null;
+        return new String[]{HologramManager.getIDFromHolo(holo)};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return hologram.toString(e, debug) + "'s id";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        hologram = (Expression<Hologram>) exprs[0];
+        return true;
+    }
+}
