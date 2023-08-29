0c20089ce5c511ebb382ea0ba079d7ea9a02a235
Ankoki-Dev <ankoki.mc@gmail.com>
2023-08-29 13:04:01 +0100

    Bug fix.
    Took 36 minutes

--- src/main/java/com/ankoki/skjade/utils/ReflectionUtils.java
+++ src/main/java/com/ankoki/skjade/utils/ReflectionUtils.java
@@ -38,7 +38,9 @@ public class ReflectionUtils {
 
     public static void sendPacket(Player player, Object... packets) {
         try {
-            Object connection = playerConnection.get(getHandle.invoke(player));
+            Object handle = getHandle(player);
+            playerConnection.setAccessible(true);
+            Object connection = playerConnection.get(handle);
             for (Object packet : packets) {
                 sendPacket.invoke(connection, packet);
             }
